387912ffd6afeb9d5a5880462833e98cda829e05
Paul Pessoa <paulmspessoa@gmail.com>
2022-04-27 23:21:47 -0300

    ajuste botoes

--- src/layout/Content.js
+++ src/layout/Content.js
@@ -1,56 +1,60 @@
 import React from 'react';
 import styles from './Content.module.css';
 
+
 function Content() {
-    function submitUrl (e) {
-        e.preventDefault()
-        alert('URL enviada para Servidor Jean')
+const saveQR = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Qr-2.svg/1024px-Qr-2.svg.png"
 
+  function submitUrl (e) {
+        e.preventDefault()
         document.getElementById("box_short_url").style.display = "flex";
-        document.getElementById("box_qrcode").style.display = "flex";
-
-
-    }
-  
-  
-    function copyButton(e) {
+      }
+      
+      function copyButton(e) {
         e.preventDefault()
+        let urlCurta = document.getElementById("url_curta");
+        urlCurta.select();
+        urlCurta.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        document.getElementById("url_curta").style.border = "#00b600 dashed 2px";
+      }
 
-            let urlCurta = document.getElementById("url_curta");
-            urlCurta.select();
-            urlCurta.setSelectionRange(0, 99999);
-            document.execCommand("copy");
-            alert("Link copiado par área de transferência: " + urlCurta.value);          
-    }
-
-  
-    return (
-    <div className={styles.content}>
+      function displayQR (e) {
+          e.preventDefault()
+          document.getElementById("box_qrcode").style.display = "flex";        
+        }
+      
+      return (
+        <div className={styles.content}>
         <div className={styles.box_content}>
+          
+          <form className={styles.form} onSubmit={submitUrl} method="post" id="" action="">
+            <input type="url" name="link" placeholder='Type or paste long URL here...' />
+            <div className={styles.custom_link}>
+                <input type="url" name="link" placeholder='curtin.com/CUSTOMLINK' />
+                <button className="buttonStandard" type='submit'>Shorten</button>
+            </div>
+          </form>
 
-      <form className={styles.form} onSubmit={submitUrl} method="post" id="" action="">
-        <input type="url" name="link" placeholder='Digite ou cole o link aqui...' />
-        <div className={styles.custom_link}>
-            <input type="url" name="link" placeholder='curtin.com/LinkPersonalizado' />
-            <button className="buttonStandard" type='submit'>Encurtar</button>
-        </div>
-      </form>
+          <div id='box_short_url' className={styles.box_short_url}>
+            <input id="url_curta" type="text" name="link" placeholder='curtin.com/CUSTOMLINK'/>
+            <button onClick={copyButton}>Copy</button>
+            <button onClick={copyButton}>Clear</button>
+            <button onClick={displayQR}>QR Code</button>
+          </div>
 
-      <div id='box_short_url' className={styles.box_short_url}>
-        <input id="url_curta" type="text" name="link" placeholder='------' />
-        <button onClick={copyButton}>Copiar</button>
-      </div>
+          <div id='box_qrcode' className={styles.box_qrcode}>
+            <div className={styles.qrcode_image}>
+              <img src={saveQR}/>
+            </div>
 
-
-      <div id='box_qrcode' className={styles.box_qrcode}>
-        <div className={styles.qrcode_image}>
+            <a href={saveQR} target="_blank" type='image/png' download="image/png"> 
+              <button type='submit'>Download QR Code</button>
+            </a>  
             
-        </div>
-            <button onClick={copyButton}>Baixar QRCODE</button>
-      </div>
+          </div>
 
         </div>
-
     </div>
   );
 }
