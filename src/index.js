3f9dfb0efffe0ef358d08a6cd021baa7aa5f4393
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-19 01:52:42 -0300

    install PWA function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +10,5 @@ root.render(
   </React.StrictMode>
 );
 
+
+serviceWorker.register();
