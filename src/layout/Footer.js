9a0c5ef4ef9c5998508770f179725188a8d3ae18
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-19 20:20:08 -0300

    remove h1

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -5,8 +5,12 @@ function Footer () {
     return (
         <div className={styles.container}>
             <p>Developed by</p>
-            <a href="https://devjean.com" target="_blank" rel="noopener noreferrer"> Jean Carlos (Back) </a>  
-            <a href="https://github.com/paulpessoa" target="_blank" rel="noopener noreferrer"> Paul Pessoa (Front) </a>
+              
+            <span>
+                <a href="https://devjean.com" target="_blank" rel="noopener noreferrer"> Jean Carlos (Back) </a>
+                 | 
+                <a href="https://github.com/paulpessoa" target="_blank" rel="noopener noreferrer"> Paul Pessoa (Front) </a> 
+            </span>
         </div>
     )
 }
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './Header.module.css';
-import logo from '../image/logo-radius.png'
+import logo from '../image/logo-bg.png'
 
 function Header () {
     return (
         <div className={styles.container}>
             <div className={styles.navmenu}>
-                <img className={styles.logo} src={logo} alt="oi"/>
-                <h1>Link Shortener</h1>
+                <a href="https://github.com/paulpessoa/curtin" target="_blank" rel="noopener noreferrer">
+                    <img className={styles.logo} src={logo} alt="oi"/>
+                    {/* <h1>Link Shortener</h1> */}
+                </a>
             </div>
         </div>
     )
